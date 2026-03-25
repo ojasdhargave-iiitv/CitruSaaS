1d5fdd00f8120e295026922329c8d6121467294f
ojasdhargave-iiitv <202451120@iiitvadodara.ac.in>
2026-03-25 11:43:55 +0530

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import app from "./app.js";
 
-// const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port: http://localhost:${PORT}`);
